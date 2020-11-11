c2310baea572029f86c49bada5f42f64e22938ed
posol <dmdmasusapjira1@gmail.com>
2020-11-11 15:48:35 +0000

    add logger

---
+++ src/utils/error.js
@@ -0,0 +1,10 @@
+module.exports = class AppError extends Error {
+  constructor(message, httpCode) {
+    super(message);
+    // Saving class name in the property of our custom error as a shortcut.
+    this.name = this.constructor.name;
+    // Capturing stack trace, excluding constructor call from it.
+    Error.captureStackTrace(this, this.constructor);
+    this.httpCode = httpCode || 500;
+  }
+};
