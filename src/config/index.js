b8b729ba3f4c64f9db57cd2f4ca0a53c34540590
posol <dmdmasusapjira1@gmail.com>
2020-11-11 15:48:13 +0000

    fix config

--- src/config/index.js
+++ src/config/index.js
@@ -7,7 +7,10 @@ if (envFound.error) {
 }
 
 module.exports = {
-  azsListEndpoint: `${process.env.BASE_API_URL}${process.env.AZS_LIST_API}${process.env.API_KEY}`,
+  stationsEndpoint: `${process.env.BASE_API_URL}${process.env.STATIONS_API}${process.env.API_KEY}`,
   pricesEndpoint: `${process.env.BASE_API_URL}${process.env.PRICES_API}${process.env.API_KEY}`,
   port: process.env.PORT,
+  httpTimeOut: process.env.HTTP_TIMEOUT,
+  httpRetryCount: process.env.HTTP_RETRY_COUNT,
+  logsLevel: process.env.LOGS_LEVEL,
 };
